2d2a5fed2f35634c67879c90af3a16fcfe944c8f
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-11-24 23:43:51 +0200

    spl_assistant route

---
+++ design/slp_assistant.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+""" This module starts a Flask web application
+spl_assistant route"""
+
+from flask import Flask, render_template
+from models import storage
+from models.age_range import AgeRange
+
+app = Flask(__name__)
+
+
+# @app.teardown_appcontext
+# def teardown_db(error):
+#     """ close storage """
+#     storage.close()
+
+
+@app.route('/slp_assistant', strict_slashes=False)
+def categories():
+    """ display AgeRange objects """
+    age_ranges = storage.all(AgeRange).values()
+    return render_template(
+        'index.html', age_ranges=age_ranges) + render_template(
+            'result.html', age_ranges=age_ranges)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
