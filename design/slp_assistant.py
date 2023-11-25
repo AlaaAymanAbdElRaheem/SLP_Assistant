0209fa06c8c7c400e3a1c8098f73805a802323f1
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-11-25 18:54:25 +0200

    slp_assistant/result route

--- design/slp_assistant.py
+++ design/slp_assistant.py
@@ -6,6 +6,7 @@ spl_assistant route"""
 from flask import Flask, render_template
 from models import storage
 from models.age_range import AgeRange
+from models.category import Category
 
 app = Flask(__name__)
 
@@ -21,6 +22,14 @@ def index_route():
     """ display AgeRange objects """
     age_ranges = storage.all(AgeRange).values()
     return render_template('index.html', age_ranges=age_ranges)
+
+@app.route('/slp_assistant/result', strict_slashes=False)
+def result_route():
+    """ display AgeRange objects """
+    age_ranges = storage.all(AgeRange).values()
+    categories = storage.all(Category).values()
+    return render_template('result.html', age_ranges=age_ranges, categories=categories)
+
 
 
 if __name__ == '__main__':
