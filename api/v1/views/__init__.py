33aee61f321f6452c4921f34fe28e272be295d06
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2023-11-29 16:29:19 +0200

    update

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -10,4 +10,3 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 if app_views is not None:
     from api.v1.views.milestones import *
-    from api.v1.views.age_range_id import *
